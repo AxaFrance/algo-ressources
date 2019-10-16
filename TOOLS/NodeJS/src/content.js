2660b1920e46028109bc46c9df46f81ef3fcbbed
Johnathan MEUNIER <jonathan.meunier@axa.fr>
2019-10-16 14:29:53 +0200

    clean content js

--- TOOLS/NodeJS/src/content.js
+++ TOOLS/NodeJS/src/content.js
@@ -14,17 +14,5 @@ const logs = (...args) => console.error(args);
 //console.error( $variable );
 //return to send result
 export default (input) => {
-    const [width, height] = input.shift().split(' ');
-    const grid = toGrid(input);
-    let mouv = 0;
-    let x = 0, y = -1;
-    while(y < height){
-        while(x < width){
-            if(grid[y + 1][x] != 'X'){ // Test descente
-                y += 1;
-                mouv ++;
-            }
-        }
-    }
-    return mouv - 1;
+
 };
