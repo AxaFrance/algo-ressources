1713acc6c5422c57f0abc8ed8b4b0f08cedb5811
Thibault Ballier <thibault.ballier@axa.fr>
2019-10-16 15:29:01 +0200

    solutions thibault

--- TRAINING/creditAgricole2019/powerPlant/thibault.py
+++ TRAINING/creditAgricole2019/powerPlant/thibault.py
@@ -5,33 +5,6 @@ for line in sys.stdin:
     lines.append(line.rstrip('\n'))
 sys.stderr.write(str(lines) + "\n\n")
 
-# lines = ['5 5', 'X..X.', 'X...X', '..XXX', 'X...X', '.X.X.']
-
-
-# lines = """
-# 20 12
-# ..X.X..X....
-# ...XX.....X.
-# ..X....XX..X
-# X......X....
-# .....X....X.
-# .X.....X....
-# X..X......XX
-# .....XX.....
-# ...XXXX.X...
-# .X.X.XX.X...
-# X..X....X..X
-# .X....XX..X.
-# X.X.........
-# ..X...X.X..X
-# X......X...X
-# .......XXXXX
-# X...XX.X...X
-# .XX..X....XX
-# ..X.X..XX...
-# XX....XX..XX
-# """.strip().split("\n")
-
 N, M = list(map(int, lines[0].split(" ")))
 lines = lines[1:]
 grid = [list(line) for line in lines]
@@ -41,27 +14,6 @@ def is_usable(line):
     if line == "X" * len(line):
         return False
     return True
-
-
-# def compute_moves(line: int, col: int):
-#     if line >= N:
-#         return 0
-#     if not 0 <= col < M:
-#         return -1
-#     if lines[line][col] == 'X':
-#         return -1
-#     a = compute_moves(line + 1, col)
-#     if a != -1:
-#         return a
-#     r = [
-#         compute_moves(line, col + 1),
-#         compute_moves(line, col - 1),
-#     ]
-#
-#     m = min(r)
-#     if m == -1:
-#         return -1
-#     return m + 1
 
 
 if not is_usable(lines[0]) or not is_usable(lines[-1]):
@@ -80,9 +32,6 @@ def get(line: int, col: int):
     return int(g)
 
 
-# has_changed = True
-# while has_changed:
-has_changed = False
 for line in range(N - 1, -1, -1):
     rrange = list(range(M))
     for col in rrange + rrange[::-1]:
@@ -99,9 +48,7 @@ for line in range(N - 1, -1, -1):
             if r == math.inf:
                 grid[line][col] = math.inf
             else:
-                if grid[line][col] != r + 1:
-                    has_changed = True
-                    grid[line][col] = r + 1
+                grid[line][col] = r + 1
 
 filtered = filter(lambda e: e != 'X' and e != math.inf, grid[0])
 if len(list(filtered)) < 1:
