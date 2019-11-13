62f7a96e3de140cd80bd84c6a901b748cc6250e4
Thibault Ballier <thibault.ballier@axa.fr>
2019-11-13 13:58:55 +0100

    solutions thibault

---
+++ TRAINING/2019/devChampion/toboggan_thibault.py
@@ -0,0 +1,13 @@
+#*******
+#* Read input from STDIN
+#* Use: echo or print to output your result to STDOUT, use the /n constant at the end of each result line.
+#* Use: sys.stderr.write() to display debugging information to STDERR
+#* ***/
+import sys
+
+lines = []
+for line in sys.stdin:
+    lines.append(line.rstrip('\n'))
+
+lines = lines[1:]
+print(len(list(filter(lambda a: 5 <= a <= 9, (map(int, lines[0].split(" ")))))))
