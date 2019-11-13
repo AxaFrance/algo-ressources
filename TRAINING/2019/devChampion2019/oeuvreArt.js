2c6cff592082d70f7f0de6868fb0a7fbf21be558
Johnathan MEUNIER <jonathan.meunier@axa.fr>
2019-11-13 13:59:09 +0100

    move to 2019 dev champion

--- TRAINING/2019/devChampion2019/oeuvreArt.js
+++
@@ -1,8 +0,0 @@
-input.shift();
-const colors = [2,3,5,7,11];
-const res = new Set();
-input.forEach(e => {
-
-	colors.filter(c => (e/1)%c === 0).forEach(e => res.add(e));
-});
-console.log([...res].sort((a,b) => a-b).join(" "));
--- TRAINING/2019/devChampion2019/touchesBoguees.js
+++
@@ -1,6 +0,0 @@
-input.shift();
-const a = input.shift().split(" ");
-const b = input.shift().split(" ");
-let m = a.reduce((acc,v, i) => ({...acc, [v]: b[i], [v.toUpperCase()]: b[i].toUpperCase()}), {});
-
-console.log(input.shift().reduce((a,v) => a+(m[v] || v), ""));
