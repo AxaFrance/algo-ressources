d7cbb2c62e6074d87df060ba1bee8451ab3876d4
Thibault Ballier <me@tbmc.fr>
2020-09-25 11:23:54 +0200

    battleDev 03/20 kotlin exo 1

---
+++ TRAINING/2020/battleDev0320/kotlin-thibault/exercice1/fr.tbmc/src/main.kt
@@ -0,0 +1,22 @@
+/*******
+ * Read input from System.in
+ * Use: println to ouput your result to STDOUT.
+ * Use: System.err.println to ouput debugging information to STDERR.
+ * ***/
+import java.util.*
+import java.math.*
+import kotlin.text.*
+
+fun main(args : Array<String>) {
+    val input = generateSequence(::readLine)
+    val result = input
+            .drop(0)
+            .groupBy { it.toLowerCase() }
+            .map { it.key to it.value.size }
+            .toList()
+            .sortedByDescending { it.second }
+            .take(2)
+            .joinToString(" ") { it.first }
+    println(result)
+
+}
