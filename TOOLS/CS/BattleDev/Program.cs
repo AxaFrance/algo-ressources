bbe453ed72665d2cab26e204406288684c3f094d
FIRLEJ Jerome <A119HS@login.axa>
2022-11-08 14:23:04 +0100

    feat(csharp): ajout de fonctions utiles

--- TOOLS/CS/BattleDev/Program.cs
+++ TOOLS/CS/BattleDev/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace BattleDev
 {
     public class Program
     {
-        public static void Main()
+
+        private int[] GetIntsLine() => Console.ReadLine().Split(" ").Select(v => Int32.Parse(v)).ToArray();
+        private int GetIntLine() => Int32.Parse(Console.ReadLine());
+        public static void Main(string[] args)
         {
             string line;
             while ((line = Console.ReadLine()) != null)
